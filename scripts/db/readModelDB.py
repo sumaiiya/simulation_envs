b2fc716d3f8b9ee9894739faabbeaabb02a4ab63
sumaiiya <40636530+sumaiiya@users.noreply.github.com>
2025-05-09 13:36:38 +0200

    Update readModelDB.py

--- scripts/db/readModelDB.py
+++ scripts/db/readModelDB.py
@@ -71,7 +71,7 @@ def createMetabolome(db, mediaName, pH0=6.4, pHFunc=None):
     return Metabolome(metabolites = metObjs, pH = pH0, pHFunc = pHFunc)
 
 
-
+"""
 @db_session()
 def createFeedingTerm(db, feedingTermID, mediaName):
     metabolites = query_string(db, 'feedingTerms2metabolites', 'feedingTerm', feedingTermID)
@@ -92,12 +92,12 @@ def createFeedingTerm(db, feedingTermID, mediaName):
     term = {i[2]:(i[3], i[4]) for i in metabolites}  # i[2] is metabolite name
     
     for i in metabolome.metabolites:
-        if i.name not in term:  # ✅ check by name
-            term[i.name] = (0, 0)
-    
+    if i not in term:  # Check if the metabolite name is not already in the dictionary
+        term[i] = (0, 0)
+
     return FeedingTerm(id = feedingTermID, metDict = term)
 
-"""
+
 
 @db_session()
 def createSubpopulation(db, supopulationID, mediaName):
